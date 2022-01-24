4a993f567869e1e27f8a659752361e4a1aa5de87
TomJKing <tom.king@nationalarchives.gov.uk>
2022-01-24 14:33:19 +0000

    Upgrade sbox-ecr Terraform to v1.1.3

--- terraform/ecr-sandbox/root.tf
+++ terraform/ecr-sandbox/root.tf
@@ -6,12 +6,7 @@ resource "aws_ecr_repository" "sandbox_ecr_repository" {
   }
 }
 
-resource "aws_iam_policy" "ecr_cross_account_permission" {
-  name   = "TDRECRCrossAccountPermission${title(local.environment)}"
-  policy = templatefile("${path.module}/templates/ecr_cross_account_policy.json.tpl", { account_id = data.aws_ssm_parameter.management_account.value })
-}
-
 resource "aws_ecr_repository_policy" "mgmt_account_permission" {
-  policy     = aws_iam_policy.ecr_cross_account_permission.policy
   repository = aws_ecr_repository.sandbox_ecr_repository.name
+  policy     = templatefile("${path.module}/templates/ecr_cross_account_policy.json.tpl", { account_id = data.aws_ssm_parameter.management_account.value })
 }
