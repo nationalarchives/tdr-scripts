776c3d2f3b6925439eb288a55cb7f619bd2d5bde
MancunianSam <sam.palmer@nationalarchives.gov.uk>
2022-02-25 09:49:32 +0000

    Add the time to the branch name

--- dependabot-merge/merge.py
+++ dependabot-merge/merge.py
@@ -3,6 +3,7 @@ import requests
 import subprocess
 import os
 import json
+from datetime import datetime
 
 headers = {'Authorization': 'bearer ' + os.environ["GITHUB_TOKEN"]}
 working_directory = os.getcwd()
@@ -25,6 +26,7 @@ dependabot_prs = [pr for pr in pull_requests if pr_filter(pr)]
 version_updates = []
 for dependabot_pr in dependabot_prs:
     sha = dependabot_pr["head"]["sha"]
+    print(sha)
     a = subprocess.run(f"git diff master..{sha} -- package.json",
                        cwd=working_directory, capture_output=True, shell=True)
     lines = a.stdout.decode("utf-8").split("\n")
@@ -46,9 +48,11 @@ with open(working_directory + '/package.json', 'w') as writer:
     writer.write(json.dumps(package_json, indent=2) + "\n")
 
 subprocess.run("npm i -package-lock-only", cwd=working_directory, shell=True)
-subprocess.run("git checkout -b dependabot-merged-changes", cwd=working_directory, shell=True)
+now = datetime.now().strftime("%Y-%m-%dT%H-%M")
+subprocess.run(f"git checkout -b dependabot-merged-changes-{now}", cwd=working_directory, shell=True)
 subprocess.run("git add -A", cwd=working_directory, shell=True)
 subprocess.run("git commit -m 'Merged dependabot changes'", cwd=working_directory, shell=True)
-subprocess.run("git push -u origin dependabot-merged-changes", cwd=working_directory, shell=True)
-pr_json = {"head": "dependabot-merged-changes", "base": "master", "title": "Merged dependabot updates"}
+subprocess.run(f"git push -u origin dependabot-merged-changes-{now}", cwd=working_directory, shell=True)
+pr_json = {"head": f"dependabot-merged-changes-{now}", "base": "master", "title": f"Merged dependabot updates {now}"}
 res = requests.post(url, json=pr_json, headers=headers)
+print(res)
