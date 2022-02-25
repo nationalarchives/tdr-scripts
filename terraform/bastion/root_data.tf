12ab9e9a487dfc78edf42b1a2abfd7e049101af2
MancunianSam <sam.palmer@nationalarchives.gov.uk>
2022-02-25 12:49:38 +0000

    Fix terraform formatting

--- terraform/bastion/root_data.tf
+++ terraform/bastion/root_data.tf
@@ -2,7 +2,7 @@ data "aws_ssm_parameter" "cost_centre" {
   name = "/mgmt/cost_centre"
 }
 
-data "aws_ami" amazon_linux_ami {
+data "aws_ami" "amazon_linux_ami" {
   owners      = ["amazon"]
   name_regex  = "^amzn2-ami-hvm-2.0.\\d{8}.0-x86_64-gp2$"
   most_recent = true
