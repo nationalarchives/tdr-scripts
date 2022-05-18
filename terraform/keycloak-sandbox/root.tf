3aeda3314952a7158159d31932a19189b92057c8
MancunianSam <sam.palmer@nationalarchives.gov.uk>
2022-05-18 09:57:14 +0100

    Use latest tag instead of test

--- terraform/keycloak-sandbox/root.tf
+++ terraform/keycloak-sandbox/root.tf
@@ -67,7 +67,7 @@ data "template_file" "app" {
 
   vars = {
     # Deploy the latest integration image
-    app_image                         = "${aws_ecr_repository.ecr_repository.repository_url}:test"
+    app_image                         = "${aws_ecr_repository.ecr_repository.repository_url}:latest"
     app_port                          = 8080
     app_environment                   = var.environment
     aws_region                        = local.aws_region
