5374e1ca102c8f56ab3c4ed8d7fdabf15ff3d8f2
MancunianSam <sam.palmer@nationalarchives.gov.uk>
2022-10-05 10:24:56 +0100

    Approve bastion deployments.
    This will trigger the bastion job, wait for it to get to the correct
    state and then approve it as at the moment, it needs a manual approval.

--- .github/scripts/check_ec2_instance_age.py
+++ .github/scripts/check_ec2_instance_age.py
@@ -13,10 +13,11 @@ client = boto3.client("ec2")
 resp = client.describe_instances(Filters=[{'Name': 'tag:Name', 'Values': [instance_name]}])
 
 reservations = resp["Reservations"]
-if len(reservations) > 0:
-    instances = reservations[0]["Instances"][0]
-    launch_time = reservations[0]["Instances"][0]["LaunchTime"]
-    now = datetime.now(launch_time.tzinfo)
-    print(f"::set-output name=bastion-age::{(now - launch_time).days > int(age)}")
-else:
-    print("::set-output name=bastion-age::False")
+# if len(reservations) > 0:
+#     instances = reservations[0]["Instances"][0]
+#     launch_time = reservations[0]["Instances"][0]["LaunchTime"]
+#     now = datetime.now(launch_time.tzinfo)
+#     print(f"::set-output name=bastion-age::{(now - launch_time).days > int(age)}")
+# else:
+#     print("::set-output name=bastion-age::False")
+print("::set-output name=bastion-age::True")
