146ba128600c727139d6f20e3960025ccf8eb12a
TomJKing <tom.king@nationalarchives.gov.uk>
2024-10-23 07:56:37 +0100

    Restructure README

--- terraform/restore-database/root.tf
+++ terraform/restore-database/root.tf
@@ -6,11 +6,10 @@ resource "aws_ssm_parameter" "database_url" {
   name      = "/${local.environment}/${local.db_name}/database/url"
   type      = "SecureString"
   value     = aws_db_instance.restore_db_instance.endpoint
-  overwrite = true
 }
 
 resource "random_string" "identifier" {
-  length  = 4
+  length  = 10
   upper   = false
   special = false
 }
@@ -21,22 +20,23 @@ resource "aws_db_instance" "restore_db_instance" {
     use_latest_restorable_time    = var.restore_time == "" ? true : null
     restore_time                  = var.restore_time == "" ? null : var.restore_time
   }
+  identifier_prefix                   = var.database
+  identifier                          = random_string.identifier.result
   instance_class                      = "db.t3.medium"
   db_subnet_group_name                = data.aws_db_subnet_group.subnet_group.name
   db_name                             = var.restore_time == "" ? local.db_name : null
   final_snapshot_identifier           = "restored-${var.database}-final-snapshot-${random_string.identifier.result}-${local.environment}"
   iam_database_authentication_enabled = true
   vpc_security_group_ids              = [data.aws_security_group.db_security_group.id]
+  availability_zone                   = var.instance_availability_zone
 }
 
 resource "aws_iam_policy" "iam_db_authentication_policy" {
-  count  = local.attach_new_policy_count
   policy = templatefile("./tdr-terraform-modules/iam_policy/templates/restored_db_access.json.tpl", { cluster_arn = "arn:aws:rds-db:${local.aws_region}:${var.tdr_account_number}:dbuser:${aws_db_instance.restore_db_instance.resource_id}/${local.user}" })
   name   = "RestoredDbAccessPolicy${title(local.environment)}"
 }
 
 resource "aws_iam_role_policy_attachment" "attach_db_authentication_policy" {
-  count      = local.attach_new_policy_count
   policy_arn = aws_iam_policy.iam_db_authentication_policy[count.index].arn
   role       = data.aws_iam_role.task_role.id
 }
--- terraform/restore-database/root_locals.tf
+++ terraform/restore-database/root_locals.tf
@@ -2,7 +2,6 @@ locals {
   environment             = terraform.workspace
   db_name                 = replace(var.database, "-", "")
   user                    = local.db_name == "consignmentapi" ? "consignment_api_user" : "keycloak_user"
-  attach_new_policy_count = var.database == "consignment-api" ? 1 : 0
   aws_region              = "eu-west-2"
   assume_role             = "arn:aws:iam::${var.tdr_account_number}:role/TDRRestoreDbTerraformRole${title(local.environment)}"
   ecs_task_role           = local.db_name == "keycloak" ? "KeycloakECSTaskRole${local.environment}" : "${local.db_name}_ecs_task_role_${local.environment}"
